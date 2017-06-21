29b037eae3197f7fcaa44c699bfc5691ad29daa1
cjewell47 <charles.jewell47@gmail.com>
2017-06-21 15:23:08 +0100

    robot-name passing all tests

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -1,15 +1,24 @@
 const Robot = function() {
-  this.name = `${randomLetter()}` + `${randomLetter()}` + `${randomNumber()}`;
+  this.start = () => {
+    this.startName = generateName();
+    this.startName in usedNames ? this.start() : this.name = this.startName;
+    usedNames[this.name] = true;
+  };
 
   this.reset = () => {
     usedNames[this.name] = true;
-    this.newName = `${randomLetter()}` + `${randomLetter()}` + `${randomNumber()}`;
+    this.newName = generateName();
     this.newName in usedNames ? this.reset() : this.name = this.newName;
   };
+  this.start();
 };
 
 const usedNames = {};
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+function generateName() {
+  return `${randomLetter()}` + `${randomLetter()}` + `${randomNumber()}`;
+}
 
 function randomLetter() {
   return letters.charAt(Math.floor(Math.random() * letters.length));
