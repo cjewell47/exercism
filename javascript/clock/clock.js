c2689b4534f7653cbb8cd76ce9ac87caa3977c51
cjewell47 <charles.jewell47@gmail.com>
2017-06-27 20:26:06 +0100

    32 of 50 tests passing for clock in JS

--- javascript/clock/clock.js
+++ javascript/clock/clock.js
@@ -1,19 +1,63 @@
-var at = function(h, m) {
-  if(h >= 24) {
-    h -= 24;
+
+var Clock = function(hour, min) {
+  this.hour = hour;
+  if (min === undefined) {
+    min = 0;
   }
-  if( m && m >= 60) {
-    m -= 60;
+  this.min = min;
+  this.hour %= 24;
+  if(this.min >= 60) {
+    this.hour += Math.floor(this.min / 60);
+    this.hour %= 24;
   }
-  if(h < 10) {
-    '0' + h;
-  }
-  if(m && m < 10) {
-    '0' + m;
-  }
-  // if(!m) {
-  //   m = '00';
-  // }
+  this.min  %= 60;
+  this.hourString = () => {
+    return correctDigits(this.hour);
+  };
+  this.minString = () => {
+    return correctDigits(this.min);
+  };
+  this.toString = () => {
+    return this.hourString() + ':' + this.minString();
+  };
+  this.plus   = plus;
+  this.minus  = minus;
+  this.equals = equals;
 };
 
-module.exports = at;
+function plus(min) {
+  this.min += min;
+  this.hour += Math.floor(this.min / 60);
+  this.hour %= 24;
+  this.min %= 60;
+  return this;
+}
+
+function minus(min) {
+  var hour = Math.floor(min / 60);
+  min = min % 60;
+  if (this.min < min) {
+    this.min = this.min + 60 - min;
+    this.hour = (this.hour + 24 - 1 - hour) % 24;
+  } else {
+    this.min  -= min;
+    this.hour -= hour;
+  }
+  return this;
+}
+
+function equals(clock) {
+  return this.toString() === clock.toString();
+}
+
+function correctDigits(n) {
+  var s = String(n);
+  s.length === 1 ? s = '0' + s : s;
+  return s;
+}
+
+function at(hour, min) {
+  return new Clock(hour, min);
+}
+
+module.exports.at = at;
