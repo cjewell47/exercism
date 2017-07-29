f8d1c7ee0b33f88f17ebeb9fe79f02f8efb576c7
cjewell47 <charles.jewell47@gmail.com>
2017-07-29 22:09:55 +0100

    ruby difference of squares complete

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -4,15 +4,15 @@ class Squares
   end
 
   def square_of_sum
-    @square_of_sum = numbers.reduce { |a, b| a + b**2 }
+    numbers.reduce(:+) **2
   end
 
   def sum_of_squares
-    @sum_of_squares = numbers.reduce(:+) **2
+    numbers.reduce { |a, b| a + b**2 }
   end
 
   def difference
-    (square_of_sum - sum_of_squares).abs
+    (square_of_sum - sum_of_squares)
   end
 
   private
