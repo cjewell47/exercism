51a7e8ca85370c3aedc0d3f86aaa33eb8fff68ce
cjewell47 <charles.jewell47@gmail.com>
2017-08-02 17:11:47 +0100

    refactored Binary in JS

--- javascript/binary/binary.js
+++ javascript/binary/binary.js
@@ -5,15 +5,14 @@ const Binary = function(num) {
 
 const toDecimal = function() {
   const length = this.num.length;
-  const digits = this.num.split('');
   let decimal  = 0;
   for(let i = length; i > 0; i--){
-    if(digits[i-1] !== '0' && digits[i-1] !== '1') {
+    if(this.num[i-1] !== '0' && this.num[i-1] !== '1') {
       return 0;
     } else {
       const power      = length - i;
       const multiplier = Math.pow(2, power);
-      decimal += (digits[i-1] * multiplier);
+      decimal += (this.num[i-1] * multiplier);
     }
   }
   return decimal;
