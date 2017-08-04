0974f655c8bd5c3a43689499dab423625bd1f3e4
cjewell47 <charles.jewell47@gmail.com>
2017-08-04 15:55:04 +0100

    prime factors in JS all tests passing

--- javascript/prime-factors/prime-factors.js
+++ javascript/prime-factors/prime-factors.js
@@ -1,7 +1,17 @@
-const primeFactors = function() {
-  this.for = function(x) {
+const primeFactors = {};
 
-  };
+primeFactors.for = function(x) {
+  const factors = [];
+  let factor    = 2;
+  while(x !== 1) {
+    if(x % factor === 0) {
+      factors.push(factor);
+      x /= factor;
+    } else {
+      factor++;
+    }
+  }
+  return factors;
 };
 
 module.exports = primeFactors;
